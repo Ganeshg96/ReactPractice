fa0621d1d3d5671bc212b0a13c2d78d4640b7803
ganesh gone <ganeshgone2003@gmail.com>
2024-07-31 22:25:01 +0530

    created meme generator

--- src/App.js
+++ src/App.js
@@ -1,96 +1,19 @@
 import React from "react"
+import Header from "./components/Header"
+import Meme from "./components/Meme"
 import './style.css'
-
+/**
+ * Challenge: 
+ * - Create a Meme component.
+ * - Inside the Meme component, render a styled form
+ *   with our 2 inputs and the button.
+ * - Don't worry about adding any functionality yet
+ */
 export default function App() {
-    const [formData, setFormData] = React.useState({
-        email: "",
-        password: "",
-        passwordConfirm: "",
-        joinedNewsletter: true
-    })
-    
-    /**
-     * Challenge: Connect the form to local state
-     * 
-     * 1. Create a state object to store the 4 values we need to save.
-     * 2. Create a single handleChange function that can
-     *    manage the state of all the inputs and set it up
-     *    correctly
-     * 3. When the user clicks "Sign up", check if the 
-     *    password & confirmation match each other. If
-     *    so, log "Successfully signed up" to the console.
-     *    If not, log "passwords to not match" to the console.
-     * 4. Also when submitting the form, if the person checked
-     *    the "newsletter" checkbox, log "Thanks for signing
-     *    up for our newsletter!" to the console if passwords match.
-     */
-    
-    function handleChange(event) {
-        const {name, value, type, checked} = event.target
-        setFormData(prevFormData => ({
-            ...prevFormData,
-            [name]: type === "checkbox" ? checked : value
-        }))
-    }
-    
-    function handleSubmit(event) {
-        event.preventDefault()
-        if (formData.password===formData.passwordConfirm){
-            console.log("successfully signed up")
-        }else{
-            console.log("passwords do not match")
-            return
-        }
-
-        if(formData.joinedNewsletter){
-            console.log("thanks for signing up for newsletter")
-        }
-    }
-    
     return (
-        <div className="form-container">
-            <form className="form" onSubmit={handleSubmit}>
-                <input 
-                    type="email" 
-                    placeholder="Email address"
-                    className="form--input"
-                    name="email"
-                    onChange={handleChange}
-                    value={formData.email}
-                />
-                <input 
-                    type="password" 
-                    placeholder="Password"
-                    className="form--input"
-                    name="password"
-                    onChange={handleChange}
-                    value={formData.password}
-                />
-                <input 
-                    type="password" 
-                    placeholder="Confirm password"
-                    className="form--input"
-                    name="passwordConfirm"
-                    onChange={handleChange}
-                    value={formData.passwordConfirm}
-                />
-                
-                <div className="form--marketing">
-                    <input
-                        id="okayToEmail"
-                        type="checkbox"
-                        name="joinedNewsletter"
-                        onChange={handleChange}
-                        checked={formData.joinedNewsletter}
-                    />
-                    <label htmlFor="okayToEmail">I want to join the newsletter</label>
-                </div>
-                <button 
-                    className="form--submit"
-                >
-                    Sign up
-                </button>
-            </form>
+        <div>
+            <Header />
+            <Meme />
         </div>
     )
 }
